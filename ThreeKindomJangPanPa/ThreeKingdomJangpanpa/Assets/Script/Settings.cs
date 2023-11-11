b89955eb7deb5473da3e05d3a8fcf8f717a04e73
qkrtjddbs0103 <qkrtjddbs0103@gmail.com>
2023-11-12 00:17:54 +0900

    UI 3주차
    UI 3주차 입니다.

---
+++ ThreeKindomJangPanPa/ThreeKingdomJangpanpa/Assets/Script/Settings.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public static int health;
+    public static int power;
+    public static int speed;
+    public static int reflex;
+    public static int defence;
+    public static int invincibility;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
