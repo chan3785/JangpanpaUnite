dd48f877c03e5c5fb4795d2cea57259b5b61a4af
qkrtjddbs0103 <qkrtjddbs0103@gmail.com>
2023-12-27 20:30:00 +0900

    12/27 1차
    임시

---
+++ Assets/Script/Sword.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Enemy
+{
+    public GameObject[] hps = new GameObject[5];
+
+    private bool can_Hurt;
+
+    Animator ani;
+
+
+
+    // Start is called before the first frame update
+
+    public void walk()
+    {
+        transform.Translate(new Vector3(-walkSpeed * Time.deltaTime, 0, 0));
+    }
+
+
+    public void Attack()
+    {
+
+
+
+        ani.SetBool("Atk", true);
+
+
+
+    }
+
+    void Start()
+    {
+        hp = 5;
+        walkSpeed = 5;
+        atkSpeed = 0;
+        ani = GetComponent<Animator>();
+
+        hps[0] = GameObject.Find("HP (1)");
+        hps[1] = GameObject.Find("HP (2)");
+        hps[2] = GameObject.Find("HP (3)");
+        hps[3] = GameObject.Find("HP (4)");
+        hps[4] = GameObject.Find("HP (5)");
+        
+        
+    }
+
+    
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > -5)
+        {
+            walk();
+            can_Hurt = false;
+
+        }
+        else
+        {
+            if (timer >= atkSpeed)
+            {
+                Debug.Log("atk");
+                Attack();
+                timer = 0;
+                atkSpeed = Random.Range(1,3);
+                is_atk = true;
+                can_Hurt=true;
+            }
+            else
+            {
+                //Debug.Log("end");
+                if (timer >= 0.05f)
+                {
+                    ani.SetBool("Atk", false);
+                }
+                if (timer >= 1f)
+                {
+                    can_Hurt = false;
+                }
+                timer += Time.deltaTime;
+
+            }
+        }
+        //print(ani.GetBool("Atk"));
+        
+    }
+
+   
+
+}
