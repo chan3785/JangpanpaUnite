f08b6d82922a43762c12260565ae74e23f8860c8
DoomTak <slimhan0505@gmail.com>
2024-01-01 12:17:33 +0900

    인게임
    재업로드

--- Assets/Script/인게임코드/Player.cs
+++ Assets/Script/인게임코드/Player.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 
 
 public class Player : MonoBehaviour
 {
+    public AudioSource aud;
+
+    public AudioClip[] audios  = new AudioClip[9];
 
     public GameObject[] hps = new GameObject[3];
     public GameObject[] subHps = new GameObject[3];
@@ -66,6 +69,8 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        aud = gameObject.GetComponent<AudioSource>();
+
         hps[0] = GameObject.Find("HPBar (1)");
         hps[1] = GameObject.Find("HPBar (2)");
         hps[2] = GameObject.Find("HPBar (3)");
@@ -104,11 +109,13 @@ public class Player : MonoBehaviour
 
     private void Attack1()
     {
+        anim.SetInteger("atkType", Random.Range(1,3));
         anim.SetBool("Atk1", true);
         is_atk = true;
         atk_type = false;
         Invoke("ani_init", 0.5f);
         //Debug.Log(Atk);
+        useSubHp();
     }
 
     private void Attack2()
@@ -117,12 +124,21 @@ public class Player : MonoBehaviour
         is_atk = true;
         atk_type = true;
         Invoke("ani_init", 0.3f);
+        
+    }
 
+    private void useSubHp()
+    {
+        if (SubHp > 0)
+        {
+            hp++;SubHp--;
+        }
     }
 
     private void Activate_Attack2()
     {
         anim.SetBool("Atk2", true);
+        useSubHp();
     }
 
     private void Kill()
@@ -140,13 +156,14 @@ public class Player : MonoBehaviour
                 if (can_Parrying)
                 {
                     Debug.Log("parrying");
-
+                    anim.SetBool("succeed", true);
                 }
                 else
                 {
                     Debug.Log("guard");
                     hp -= 1;
                     subHp += 1;
+                    anim.SetBool("succeed", true);
 
                 }
             }
@@ -181,6 +198,7 @@ public class Player : MonoBehaviour
     private void Died()
     {
         Debug.Log("died");
+        SceneManager.LoadScene("MainScene");
     }
 
     private void Guard()
@@ -224,7 +242,42 @@ public class Player : MonoBehaviour
         anim.SetBool("Atk1", false);
         anim.SetBool("Atk2_", false);
         anim.SetBool("hurt", false);
+        anim.SetBool("succeed", false);
 
         is_atk = false;
+    }
+
+    private void audioParry()
+    {
+        aud.clip = audios[8];
+        if (!aud.isPlaying)
+        {
+            aud.Play();
+        }
+    }
+    
+    private void audioAtk1()
+    {
+        aud.clip = audios[1];
+        if (!aud.isPlaying)
+        {
+            aud.Play();
+        }
+    }
+    private void audioAtk2()
+    {
+        aud.clip = audios[3];
+        if (!aud.isPlaying)
+        {
+            aud.Play();
+        }
+    }
+    private void audioAtk3()
+    {
+        aud.clip = audios[5];
+        if (!aud.isPlaying)
+        {
+            aud.Play();
+        }
     }
 }
