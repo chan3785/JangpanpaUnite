add6dea26feea386379faf0886b677fea7296db6
유경선 <106594608+YuGyeongseon@users.noreply.github.com>
2024-01-03 22:26:17 +0900

    병합본 1차
    UI제외 병합해놨습니다.
    BGM 변경했습니다.

--- Assets/Script/인게임코드/Ingame_atk_Button.cs
+++ Assets/Script/인게임코드/Ingame_atk_Button.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-//using UnityEditorInternal;
+using UnityEditorInternal;
 using UnityEngine;
 
 
@@ -11,26 +11,44 @@ public class Ingame_atk_Button : MonoBehaviour
 
     private bool is_Click;
     private float time;
-    private float min_time = 0.5f;
+    private float min_time = 0.3f;
+
+    private bool ActivatelongAtk;
     // Start is called before the first frame update
+
+
+
+
     void Start()
     {
         player = GameObject.Find("ZhangFei");
-
+        ActivatelongAtk = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(is_Click)
+        if (is_Click)
         {
             time += Time.deltaTime;
+            if (time > min_time && !ActivatelongAtk)
+            {
+                Debug.Log("activate: " + time);
+                ActivatelongAtk = true;
+                player.SendMessage("Activate_Attack2");
+            }
+
+
+
         }
         else
         {
             time = 0;
+            ActivatelongAtk = false;
         }
+        //Debug.Log("al:" + ActivatelongAtk);
+        //Debug.Log("l:"+longAtk);
     }
 
     public void button_Down()
@@ -41,7 +59,7 @@ public class Ingame_atk_Button : MonoBehaviour
     public void button_Up()
     {
         is_Click = false;
-        if(time > min_time)
+        if (ActivatelongAtk && time > min_time)
         {
             //Debug.Log("atk2");
             player.SendMessage("Attack2");
@@ -50,7 +68,7 @@ public class Ingame_atk_Button : MonoBehaviour
         {
             //Debug.Log("atk1");
             player.SendMessage("Attack1");
-            
+
         }
     }
 }
