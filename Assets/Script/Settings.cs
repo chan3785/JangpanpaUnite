03026867a2c98f4d7567352a11513e09420984ad
qkrtjddbs0103 <qkrtjddbs0103@gmail.com>
2024-01-04 01:47:34 +0900

    1/4 UI
    UI

--- Assets/Script/Settings.cs
+++ Assets/Script/Settings.cs
@@ -17,6 +17,7 @@ public class Settings : MonoBehaviour
     public static bool character5;
     public static bool character6;
     public static bool character7;
+    public static int cur_character;
     
     // Start is called before the first frame update
     void Start()
@@ -24,11 +25,32 @@ public class Settings : MonoBehaviour
         zhangFei = true;
         caoRen = false; caoHong = false;
         zhangLiao= false;
+        cur_character = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ZhangFei() 
+    {
+        cur_character = 1;
+    }
+
+    public void CaoRen() 
+    {
+        cur_character = 2;
+    }
+
+    public void CaoHong() 
+    {
+        cur_character = 3;
+    }
+
+    public void ZhangLiao() 
+    {
+        cur_character = 4;
     }
 }
