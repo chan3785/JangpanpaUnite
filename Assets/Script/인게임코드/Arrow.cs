c90b7674192a400db7e9374a4665af524198c768
qkrtjddbs0103 <qkrtjddbs0103@gmail.com>
2024-03-09 10:06:24 +0900

    3/9 UI
    UI

---
+++ Assets/Script/인게임코드/Arrow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Arrow_Fall()
+    {
+        bossCaoHong.arrow2_fall = true;
+        //Debug.Log("arrow fall");
+    }
+}
--- Assets/Script/인게임코드/bossCaoHong.cs
+++ Assets/Script/인게임코드/bossCaoHong.cs
@@ -16,8 +16,8 @@ public class bossCaoHong : MonoBehaviour
 
     public GameObject player;
     private GameObject boss;
-    public GameObject arrow1;
-    public GameObject arrow2; 
+    public GameObject arrow1; // 1번공격 화살
+    public GameObject arrow2; // 2번공격 화살
     public GameObject CaoHong;
     Vector3 position;
 
@@ -40,16 +40,19 @@ public class bossCaoHong : MonoBehaviour
 
     public int parryProbability;
 
-    public int distance, arrow_speed;
-    public float arrow;
-    private float arrow_parry_timer, arrow_shield_timer;
-    public float arrow_location;
-    private float arrow_timer;
-    private float shield_timer;
+    public int distance, arrow_speed; // 화살 관련 변수들
+    private float arrow_timer; // 화살이 발사된 후 소요된 시간을 기록
+    private float shield_timer; // 방어버튼 눌리는 시간을 기록
     private float arrow1_approachingTime;
-    private bool isGuard;
-    private bool arrow1_shot;
-    private bool can_Parrying;
+    private float arrow2_approachingTime; // 화살이 플레이어에게 도달하는 시간을 저장하는 변수로서 start함수에서 초기화 됨
+    private bool isGuard; // 방어버튼이 눌러져 있으면 true가 됨
+    private bool arrow1_shot; 
+    private bool arrow2_shot; // 화살이 발사되는 순간ㄴ true가 되고 화살이 플레이어에게 도달하면 false가 됨
+    public static bool arrow2_fall; // 불화살이 떨어지는 애니메이션이 끝났을때 true가 됨. 관련함수: Arrow.cs의 Arrow_Fall();
+    private bool can_Parrying1;
+    private bool can_Parrying2; //패링
+
+    private int money_loss; // 조홍에게 한대 맞을때마다 플레이어가 잃는 재화의 양
 
     //public GameObject Stone;
     //private bool canThrow, canParryStone;
@@ -74,80 +77,6 @@ public class bossCaoHong : MonoBehaviour
         atkTimer += Time.deltaTime;
         //Debug.Log(atkTimer);
     }
-
-    // public void walk()
-    // {
-    //     if (!walkBack)
-    //     {
-            
-    //         if (!walkBack && (pat1 || pat2) && patTimer >= 0)
-    //         {
-    //             activatePatTimer();
-                
-    //             if (pat1)
-    //             {
-    //                 if (patTimer >= 10)
-    //                 {
-    //                     patTimer = -1;
-    //                 }
-
-    //             }
-    //             else if (pat2)
-    //             {
-    //                 if (patTimer >= 5)
-    //                 {
-    //                     if (Random.Range(1, 3) == 1)
-    //                     {
-    //                         patTimer = 0;
-    //                     }
-    //                     else
-    //                     {
-    //                         patTimer = -1;
-    //                     }
-    //                 }
-    //             }
-                
-    //         }
-    //         else
-    //         {
-    //             if (transform.position.x > -6.5)
-    //             {
-    //                 transform.Translate(new Vector3(-walkSpeed * Time.deltaTime, 0, 0));
-    //                 anim.SetBool("walk", true);
-    //                 //atk1Num = 3;
-
-    //             }
-    //             else
-    //             {
-    //                 anim.SetBool("walk", false);
-    //                 canHurt = true;
-    //                 canAtk = true;
-    //             }
-    //         }
-
-    //     }
-    //     else if (walkBack)   
-    //     {
-    //         canHurt = false;
-    //         if (transform.position.x < 1)
-    //         {
-                
-    //             anim.SetBool("walkBack", true);
-    //             if (turn)
-    //             {
-    //                 transform.Translate(new Vector3(walkSpeed * Time.deltaTime, 0, 0));
-    //             }
-    //         }
-    //         else
-    //         {
-    //             anim.SetBool("walkBack", false);
-    //             walkBack = false;
-    //             canAtk = false;
-    //             turn = false;
-    //             patTimer = 0;
-    //         }
-    //     }
-    // }
 
     private void activateArrowTimer()
     {
@@ -252,7 +181,7 @@ public class bossCaoHong : MonoBehaviour
         atk2Num--;
         Debug.Log("atk2 decreased");
     }
-    private void decrease_atk3Num()
+    private void decrease_atk3Num() // 각 atknum 변수들은 애니메이터의 변수들과 연결되어서 애니메이션 재생 횟수를 조정함
     {
         atk3Num--;
          Debug.Log("atk3 decreased");
@@ -292,7 +221,7 @@ public class bossCaoHong : MonoBehaviour
     {
         isGuard = true;
         shield_timer = arrow_timer;
-        Debug.Log("shield: " + shield_timer);
+        //Debug.Log("shield: " + shield_timer);
     }
 
     private void guardOff()
@@ -322,23 +251,23 @@ public class bossCaoHong : MonoBehaviour
         
         if(arrow1.activeSelf && arrow1_shot)
         {
-            anim.SetBool("can_attack", false);
+            anim.SetBool("can_attack", false); // 애니메이션의 can_attack이라는 bool변수로서 true가 아니면 공격 애니메이션이 작동 안함.
             activateArrowTimer();
-            if(arrow1.transform.position.x > -6)
+            if(arrow1.transform.position.x > -6) // 플레이어의 위치까지 화살 이동
             {
             arrow1.transform.Translate(new Vector3(-arrow_speed * Time.deltaTime, 0, 0));
             }
             
-            if(arrow_timer > arrow1_approachingTime)
+            if(arrow1.transform.position.x <= -6) //arrow_timer > arrow1_approachingTime
             {
                 arrow1_shot = false;
-                if(isGuard)
+                if(isGuard) // 방어버튼이 눌렸을시
                 {
-                    if(arrow1_approachingTime - shield_timer > 0 && arrow1_approachingTime - shield_timer < 4)
+                    if(arrow1_approachingTime - shield_timer > 0 && arrow1_approachingTime - shield_timer < 0.5) // 패링조건 만족할 경우 패링 함수 실행
                     {
-                        can_Parrying = true;
+                        can_Parrying1 = true; // 패링 함수는 update문에 있습니다.
                     }
-                    else
+                    else // 만족하지 않을 경우 방어 애니메이션 실행
                     {
                         Debug.Log("guard");
                         player.SendMessage("guard");
@@ -349,7 +278,7 @@ public class bossCaoHong : MonoBehaviour
                         anim.SetBool("can_attack", true);
                     }
                 }
-                else
+                else // 방어버튼이 안눌렸을시 플레이어의 hurt 함수 실행
                 {
                     actAtk1();
                     arrow_timer = 0;
@@ -366,7 +295,106 @@ public class bossCaoHong : MonoBehaviour
     {
         Debug.Log("패링 함수 작동중");
         player.SendMessage("parryOn");
-        if(arrow1.transform.position.x < 6.5f)
+        if(arrow1.transform.position.x < 6.5f) // 화살이 조홍의 위치까지 이동 
+        {
+            arrow1.GetComponent<SpriteRenderer>().flipX = false;
+            arrow1.transform.Translate(new Vector3(arrow_speed * Time.deltaTime, 0, 0));
+            Hurt("Atk1");
+        }
+        else // 위치까지 도달 후 조홍의 hurt 함수 실행
+        {
+            arrow_timer = 0;
+            shield_timer = 0;
+            arrow1.SetActive(false);
+            arrow1.GetComponent<SpriteRenderer>().flipX = true;
+            anim.SetBool("can_attack", true);
+            can_Parrying1 = false;
+        }
+    }
+
+    private void actAtk1()
+    {
+        player.SendMessage("Hurt", "nAtk");
+        atkTimer = 0;
+        if(Goods.money > 0)
+        {
+            Goods.money -= 1;
+        }
+    }
+
+    public void atk1_arrow() // 화살 오브젝트를 활성화하는 함수로서 이 스크립트가 아니라 공격 애니메이션을 잘 보시면 이벤트로 이 함수가 있을겁니다.
+    {
+        arrow1.SetActive(true);
+        arrow1_shot = true;
+    }
+
+    private void Atk2() // atk2도 atk1과 거의 같은 방식입니다.
+    {
+        anim.SetInteger("atk2", atk2Num);
+        
+        if(arrow2.activeSelf && arrow2_shot)
+        {
+            activateArrowTimer();
+            if(arrow2.transform.position.x > -7.5)
+            {
+                arrow2.transform.Translate(new Vector3(-arrow_speed * Time.deltaTime, (arrow_speed/5) * Time.deltaTime, 0));
+            }
+            else
+            {
+                arrow2.GetComponent<Animator>().Play("fall");
+                arrow2_shot = false;
+                
+                if(arrow2_fall) // arrow2_fall은 화살이 떨어지는 애니메이션이 끝났을때 true가 됩니다. Arrow 스크립트에서 true가 됨.
+                {
+                    if(isGuard)
+                    {
+                        float a = arrow2_approachingTime - shield_timer;
+                        Debug.Log("arrow2 - shield: " + a);
+                        if(arrow2_approachingTime - shield_timer > 0 && arrow2_approachingTime - shield_timer < 4)
+                        {
+                            can_Parrying2 = true;
+                            arrow2_fall = false;
+                        }
+                        else
+                        {
+                            Debug.Log("guard");
+                            player.SendMessage("guard");
+                            arrow_timer = 0;
+                            shield_timer = 0;
+                            arrow2.SetActive(false);
+                            arrow2.transform.position = new Vector3(0, 0, 0);
+                            anim.SetBool("can_attack", true);
+                            arrow2_fall = false;
+                        }
+                    }
+                    else
+                    {
+                        actAtk2();
+                        arrow_timer = 0;
+                        shield_timer = 0;
+                        arrow2.SetActive(false);
+                        arrow2.transform.position = new Vector3(0, 0, 0);
+                        anim.SetBool("can_attack", true);
+                        arrow2_fall = false;
+                    }
+                }
+            }
+        }
+    }
+
+    private void atk2_parry()
+    {
+        Debug.Log("패링 함수 작동중");
+        player.SendMessage("parryOn");
+        arrow2.transform.position = new Vector3(0, 0, 0);
+        arrow2.SetActive(false);
+        if(!arrow1.activeSelf)
+        {
+            arrow1.transform.position = new Vector3(-9, 0.5f, 0);
+        }
+        arrow1.SetActive(true);
+
+        if(arrow1.transform.position.x < 0)
         {
             arrow1.GetComponent<SpriteRenderer>().flipX = false;
             arrow1.transform.Translate(new Vector3(arrow_speed * Time.deltaTime, 0, 0));
@@ -379,78 +407,39 @@ public class bossCaoHong : MonoBehaviour
             arrow1.SetActive(false);
             arrow1.GetComponent<SpriteRenderer>().flipX = true;
             anim.SetBool("can_attack", true);
-            can_Parrying = false;
+            can_Parrying2 = false;
         }
-    }
-
-    private void actAtk1()
-    {
-        player.SendMessage("Hurt", "nAtk");
-        atkTimer = 0;
-    }
-
-    public void atk1_arrow()
-    {
-        arrow1.SetActive(true);
-        arrow1_shot = true;
-    }
-
-    private void Atk2()
-    {
-        anim.SetInteger("atk2", atk2Num);
-        // if(atk2Num > 0 && atk3Num <= 0)
-        // {
-        //     anim.Play("atk2");
-        // }
-        
-        // if(arrow2.activeSelf && arrowshot)
-        // {
-        //     activateArrowTimer();
-        //     if(arrow2.transform.position.x > -7.5)
-        //     {
-        //         arrow2.transform.Translate(new Vector3(-arrow_speed * Time.deltaTime, (arrow_speed/5) * Time.deltaTime, 0));
-        //     }
-        //     else
-        //     {
-        //         arrow2.GetComponent<Animator>().SetBool("fall", true);
-        //         Debug.Log("a");
-        //     }
-
-        //     // if(arrow_timer > arrow1_approachingTime / 2)
-        //     // {
-        //     //     arrowshot = false;
-        //     //     arrow2.transform.position = new Vector3(1, -1, 0);
-        //     //     arrow2.SetActive(false);
-        //     //     anim.SetBool("can_attack", true);
-        //     // }
-        // }
     }
 
     private void actAtk2()
     {
         player.SendMessage("Hurt", "sAtk");
         atkTimer = 0;
+        if(Goods.money > 0)
+        {
+            Goods.money -= 1;
+        }
     }
 
-    public void atk2_arrow()
+    private void atk2_arrow()
     {
-        //arrow2.SetActive(true);
+        arrow2.SetActive(true);
+        arrow2_shot = true;
     }
     
     private void Atk3()
     {
         anim.SetInteger("atk3", atk3Num);
-        //Debug.Log("atk3");
-        // if(atk3Num > 0)
-        // {
-        //     anim.Play("atk3");
-        // }
     }
 
     private void actAtk3()
     {
         player.SendMessage("Hurt", "nAtk");
         atkTimer = 0;
+        if(Goods.money > 0)
+        {
+            Goods.money -= 1;
+        }
     }
 
     private void Awake()  // ���� �ʱ�ȭ
@@ -475,13 +464,18 @@ public class bossCaoHong : MonoBehaviour
 
         position = CaoHong.transform.localPosition;
         arrow1_shot = false;
-        can_Parrying = false;
+        arrow2_shot = false;
+        can_Parrying1 = false;
+        can_Parrying2 = false;
     }
     
     private void Start()
     {
         anim.speed = atkSpeed;  
         arrow1_approachingTime = 15.5f / arrow_speed;
+        arrow2_approachingTime = 7.5f / arrow_speed;
+        money_loss = 1;
+        Goods.money = 10; // 재화 손실 테스트 용도로 임시로 초기화
     }
 
     void Update()
@@ -502,10 +496,17 @@ public class bossCaoHong : MonoBehaviour
                 Atk2();
                 Atk3();
 
-                if(can_Parrying)
+                Debug.Log("isGuard: " + isGuard);
+
+                if(can_Parrying1)
                 {
                     Debug.Log("a");
                     atk1_parry();
+                }
+
+                if(can_Parrying2)
+                {
+                    atk2_parry();
                 }
 
                 if (atk1Num <= 0 && atk2Num <= 0 && atk3Num <= 0)
@@ -516,11 +517,12 @@ public class bossCaoHong : MonoBehaviour
                     if (timer >= 1)
                     {
                         timer = 0;
-                        atk1Num = Random.Range(0, 2);
+                        //atk1Num = Random.Range(0, 2);
                         atk2Num = Random.Range(0, 2);
-                        atk3Num = Random.Range(0, 2);
+                        //atk3Num = Random.Range(0, 2);
                     }
-            }   
+                }   
+                Debug.Log("money: " + Goods.money);
         }
     }
 }
